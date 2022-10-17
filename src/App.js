0dfe6056aec62bf6e720fa1ea39bad8f5f186ef4
Ihor Kosse <2856787@gmail.com>
2022-10-17 14:23:08 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+const Tg = window.Telegram.WebApp;
 function App() {
+
+const OnClose = () => {
+    Tg.close();
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Its working
+   <button onClick={OnClose}>Close</button>
     </div>
   );
 }
